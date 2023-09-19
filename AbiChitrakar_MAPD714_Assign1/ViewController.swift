d490098538c1bf27cf1d49e94276966f8eee6ff0
Abi <abichitrakar2921@gmail.com>
2023-09-19 18:53:57 -0400

    Initial Commit

---
+++ AbiChitrakar_MAPD714_Assign1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AbiChitrakar_MAPD714_Assign1
+//
+//  Created by Abi Chitrakar on 2023-09-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
